c8bbf894c7ba046ddd3f55677f2d4512dd944aa0
Ushitora Anqou <ushitora@anqou.net>
2019-10-12 23:17:54 +0900

    [CAHP] Add tests for assembler

---
+++ llvm/test/MC/CAHP/cahp-invalid.s
@@ -0,0 +1,37 @@
+# RUN: not llvm-mc -triple cahp < %s 2>&1 | FileCheck %s
+
+# Out of range immediates
+li t0, -513 # CHECK: :[[@LINE]]:8: error: immediate must be an integer in the range [-512, 511]
+li s0, 512 # CHECK: :[[@LINE]]:8: error: immediate must be an integer in the range [-512, 511]
+addi a0, a1, 128 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [-128, 127]
+addi a0, a1, -129 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [-128, 127]
+andi a0, a1, 256 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [0, 255]
+andi a0, a1, -1 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [0, 255]
+lsli a0, a1, 16 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [0, 15]
+asri a0, a1, -1 # CHECK: :[[@LINE]]:14: error: immediate must be an integer in the range [0, 15]
+lsi a5, 32 # CHECK: :[[@LINE]]:9: error: immediate must be an integer in the range [-32, 31]
+lsi a5, -33 # CHECK: :[[@LINE]]:9: error: immediate must be an integer in the range [-32, 31]
+lui a5, 65 # CHECK: :[[@LINE]]:9: error: immediate must be an integer in the range [0, 63]
+lui a5, -1 # CHECK: :[[@LINE]]:9: error: immediate must be an integer in the range [0, 63]
+js 1024 # CHECK: :[[@LINE]]:4: error: immediate must be an integer in the range [-1024, 1023]
+js -1025 # CHECK: :[[@LINE]]:4: error: immediate must be an integer in the range [-1024, 1023]
+
+# Invalid mnemonics
+subs x1, x2 # CHECK: :[[@LINE]]:1: error: unrecognized instruction mnemonic
+nandi x14, x13 # CHECK: :[[@LINE]]:1: error: unrecognized instruction mnemonic
+
+# Invalid register names
+add foo, x1, x3 # CHECK: :[[@LINE]]:5: error: unknown operand
+sub x0, x16, x4 # CHECK: :[[@LINE]]:9: error: unknown operand
+mov t2, x8 # CHECK: :[[@LINE]]:5: error: unknown operand
+
+# Invalid operand types
+add x15, x1, 1 # CHECK: :[[@LINE]]:14: error: invalid operand for instruction
+
+# Too many operand
+mov x8, x9, x10 # CHECK: :[[@LINE]]:13: error: invalid operand for instruction
+li x11, x12, x13 # CHECK: :[[@LINE]]:9: error: invalid operand for instruction
+
+# Too few operands
+li x8 # CHECK: :[[@LINE]]:1: error: too few operands for instruction
+mov x13 # CHECK: :[[@LINE]]:1: error: too few operands for instruction
