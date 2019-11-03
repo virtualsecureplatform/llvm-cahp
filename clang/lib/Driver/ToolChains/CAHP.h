ca3417051ecf2e8d3c510caf9728a8acc8e2a608
Ushitora Anqou <ushitora@anqou.net>
2019-11-03 16:31:00 +0900

    [CAHP] Add Clang driver

---
+++ clang/lib/Driver/ToolChains/CAHP.h
@@ -0,0 +1,60 @@
+// This file is copied and modified from The LLVM Compiler Infrastructure, which
+// is distributed under the Apache License v2.0 with LLVM Exceptions (see
+// LICENSE.TXT for details). This file is licensed under the same license.
+
+#ifndef LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CAHP_H
+#define LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CAHP_H
+
+#include "Gnu.h"
+#include "clang/Driver/ToolChain.h"
+
+namespace clang {
+namespace driver {
+namespace toolchains {
+
+class LLVM_LIBRARY_VISIBILITY CAHPToolChain : public Generic_ELF {
+public:
+  CAHPToolChain(const Driver &D, const llvm::Triple &Triple,
+                 const llvm::opt::ArgList &Args)
+      : Generic_ELF(D, Triple, Args) {}
+
+  bool IsIntegratedAssemblerDefault() const override { return true; }
+
+  // No support for finding a C++ standard library yet.
+  void addLibCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
+                             llvm::opt::ArgStringList &CC1Args) const override {
+  }
+  void
+  addLibStdCxxIncludePaths(const llvm::opt::ArgList &DriverArgs,
+                           llvm::opt::ArgStringList &CC1Args) const override {}
+
+  // No default include flags.
+  void
+  AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
+                            llvm::opt::ArgStringList &CC1Args) const override {}
+
+protected:
+  Tool *buildLinker() const override;
+};
+
+} // end namespace toolchains
+
+
+namespace tools {
+namespace CAHP {
+class LLVM_LIBRARY_VISIBILITY Linker : public GnuTool {
+public:
+  Linker(const ToolChain &TC) : GnuTool("CAHP::Linker", "ld.lld", TC) {}
+  bool hasIntegratedCPP() const override { return false; }
+  bool isLinkJob() const override { return true; }
+  void ConstructJob(Compilation &C, const JobAction &JA,
+                    const InputInfo &Output, const InputInfoList &Inputs,
+                    const llvm::opt::ArgList &TCArgs,
+                    const char *LinkingOutput) const override;
+};
+} // namespace CAHP
+} // namespace tools
+} // end namespace driver
+} // end namespace clang
+
+#endif // LLVM_CLANG_LIB_DRIVER_TOOLCHAINS_CAHP_H
