0888e00ea23a27a374a58748970dbe12e52d2b37
Ushitora Anqou <ushitora@anqou.net>
2019-12-27 13:04:50 +0900

    [CAHP] Allow to pass -mcpu option to clang

--- clang/lib/Driver/ToolChains/CommonArgs.cpp
+++ clang/lib/Driver/ToolChains/CommonArgs.cpp
@@ -9,6 +9,7 @@
 #include "clang/Driver/CommonArgs.h"
 #include "Arch/AArch64.h"
 #include "Arch/ARM.h"
+#include "Arch/CAHP.h"
 #include "Arch/CSKY.h"
 #include "Arch/LoongArch.h"
 #include "Arch/M68k.h"
@@ -793,6 +794,9 @@ std::string tools::getCPUName(const Driver &D, const ArgList &Args,
   case llvm::Triple::wasm32:
   case llvm::Triple::wasm64:
     return std::string(getWebAssemblyTargetCPU(Args));
+
+  case llvm::Triple::cahp:
+    return cahp::getCAHPTargetCPU(Args);
 
   case llvm::Triple::loongarch32:
   case llvm::Triple::loongarch64:
